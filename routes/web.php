9aa4ac7026c2be2e797505809a7baec364f20136
redaOul <redaoulemhour@gmail.com>
2024-02-06 12:48:15 +0200

    Configure routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,69 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
+
+use App\Http\Controllers\loginControllers;
+use App\Http\Controllers\AccueilControllers;
+use App\Http\Controllers\DashboardControllers;
+use App\Http\Controllers\sessionControllers;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Console Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| This file is where you may define all of your Closure based console
+| commands. Each Closure is bound to a command instance allowing a
+| simple approach to interacting with each command's IO methods.
 |
 */
 
+
+// Login routes
 Route::get('/', function () {
-    return view('welcome');
+    if (Session::has('userID')) {
+        switch (Session::get('userType')) {
+            case 'adherent':
+                return redirect('/Accueil');
+                break;
+            case 'bibliothecaire':
+                return redirect('/Dashboard');
+                break;
+        }
+    } else {
+        return view('login');
+    }
+});
+Route::post('/controllers/login', [loginControllers::class, 'auth']);
+Route::post('/controllers/logout', [sessionControllers::class, 'destroySeesion']);
+
+
+// Accueil routes
+Route::get('/Accueil', function () {
+    if ((Session::has('userID')) && (Session::get('userType') == 'adherent')) return (new AccueilControllers)->loadPageData();
+    else return redirect('/');
+});
+
+Route::get('/controllers/Accueil/reserveBook/{bookId}', [AccueilControllers::class, 'reserveBook']);
+Route::get('/controllers/Accueil/searchBookByName', [AccueilControllers::class, 'searchBookByName']);
+Route::get('/controllers/Accueil/searchBookByNameLink/{bookName}', [AccueilControllers::class, 'searchBookByName']);
+Route::get('/controllers/Accueil/searchBookByCategorie/{categorieId}', [AccueilControllers::class, 'searchBookByCategorieMeth1']);
+Route::get('/controllers/Accueil/searchBookByCategorie', [AccueilControllers::class, 'searchBookByCategorieMeth2']);
+
+// Dashboard routes
+Route::get('/Dashboard', function () {
+    if ((Session::has('userID')) && (Session::get('userType') == 'bibliothecaire')) return (new DashboardControllers)->loadPageData();
+    else return redirect('/');
+});
+Route::get('/controllers/Dashboard/cancelBookReservation/{bookId}/{reservationID}', [DashboardControllers::class, 'cancelBookReservation']);
+Route::post('/controllers/Dashboard/addBook', [DashboardControllers::class, 'addBook']);
+Route::post('/controllers/Dashboard/addCategorie', [DashboardControllers::class, 'addCategorie']);
+Route::get('/controllers/Dashboard/deleteBook/{bookId}', [DashboardControllers::class, 'deleteBook']);
+
+
+
+
+Route::get('/dsf', function (){
+    return view('exploreBooks');
 });
